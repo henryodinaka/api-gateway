2567a16b7cf3413a68154e981d9ab237e4ec9481
henryodinakamin <odinaka.onah@min.ng>
2020-10-14 18:26:43 +0100

    I enabled c

---
+++ src/main/java/ng/min/gateway/config/PreFlightCorsConfiguration.java
@@ -0,0 +1,33 @@
+package ng.min.gateway.config;
+
+import io.netty.handler.codec.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class PreFlightCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        config.setAllowedOrigins(Arrays.asList("http://user.min.com.ng","http://admin.min.com.ng","http://inventory.min.com.ng","http://min.com.ng"));
+        config.addAllowedMethod(HttpMethod.PUT.name());
+        config.addAllowedMethod(HttpMethod.POST.name());
+        config.addAllowedMethod(HttpMethod.PATCH.name());
+        config.addAllowedMethod(HttpMethod.GET.name());
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
