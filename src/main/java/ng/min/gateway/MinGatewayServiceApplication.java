dd233ea5da307e85d52892ede47d064a7dfcea95
henryodinakamin <odinaka.onah@min.ng>
2020-10-14 01:51:46 +0100

    I added cors

--- src/main/java/ng/min/gateway/MinGatewayServiceApplication.java
+++ src/main/java/ng/min/gateway/MinGatewayServiceApplication.java
@@ -2,12 +2,33 @@ package ng.min.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MinGatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MinGatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("min.com.ng","inventory.min.com.ng","user.min.com.ng","admin.min.com.ng"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST","PATCH","PUT"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 
 }
